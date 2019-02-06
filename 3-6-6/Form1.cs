375fc3124f16d0c56b942b990193260e98285f52
nexys72 <nexys.stepanov@yandex.ru>
2019-02-06 12:58:44 +0500

    Добавьте файлы проекта.

---
+++ 3-6-6/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_6_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int k;
+            int i;
+            int n;
+            int l;
+            k = Convert.ToInt32(textBox1.Text);
+            i = Convert.ToInt32(textBox2.Text);
+            n = Convert.ToInt32(Math.Pow(2, i));
+            l = k * i;
+            textBox3.Text = n.ToString();
+            textBox4.Text = l.ToString();
+        }
+    }
+}
